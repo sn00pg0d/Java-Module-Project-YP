139e6a1d9d51d20303d2b39fcdd68e8428b65e9d
sn00pg0d <madmaks.45cal@gmail.com>
2022-12-29 02:14:46 +0900

    Финальная версия (всё работает)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 public class Main { // обожаю минимализм
 
-// dev branch for Y.Practicum
-public class Main {
-
     public static void main(String[] args) {
         Counter counter = new Counter();
         counter.shopCalc(); // запуск кол-ва человек и калькулятора корзины
