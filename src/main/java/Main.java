af904d80baf1fd4eb4aa0ac5de4d327c1892ad8f
sn00pg0d <madmaks.45cal@gmail.com>
2022-12-29 02:07:54 +0900

    Финальная версия (всё работает)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 public class Main { // обожаю минимализм
+
     public static void main(String[] args) {
         Counter counter = new Counter();
         counter.shopCalc(); // запуск кол-ва человек и калькулятора корзины
