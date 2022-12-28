f6def5119a113c0bccb003be1d7077feb83f2095
sn00pg0d <madmaks.45cal@gmail.com>
2022-12-28 23:57:25 +0900

    Внесение изменений во 2-м задании

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -12,15 +12,27 @@ public class Counter {
         return new Random().nextInt(r);
     }
 
-    public static int pers(int p) { // ввод с клавы количества людей, с условием - больше двух
+    public static int pers(int persons) { // ввод с клавы количества людей, с условием - больше двух
         Scanner scanner = new Scanner(System.in);
-        while (p <= 1) {
-            p = scanner.nextInt();
-            if (p <= 1) {
+        //int p;
+        int needToPay;
+        persons = 0;
+        int rand;
+        rand = randomizeIt();
+        System.out.println("Большое спасибо, что посетили наш ресторан!\n");
+        System.out.println("Ваш счёт составил: " + rand + "руб.\n");
+        System.out.println("На скольких персон делить счёт?");
+        persons = scanner.nextInt();
+        //persons = pers(persons);
+        needToPay = count(rand, persons);
+        System.out.println("Каждый из вас должен: " + needToPay + " руб.\n");
+        while (persons <= 1) {
+            persons = scanner.nextInt();
+            if (persons <= 1) {
                 System.out.println("Некорректное число, пожалуйста повторите ввод");
             }
         }
-        return p;
+        return persons;
     }
 
     public static void shopCalc() { // калькулятор суммы выбранных товаров
@@ -28,35 +40,34 @@ public class Counter {
         String shopAdd = "";
         String name = "";
         double price = 0;
-        while (price != -1) { // бесконечный цикл, с выходом через case "0"
+        System.out.println("Выберите себе игрушку из списка:");
+        System.out.println("1 - iPhone X\n2 - iPad 8\n3 - iPods 3\n0 или Завершить - Выход");
+        while (price != -1) { // бесконечный цикл, с выходом через "завершить"
             shopAdd = scanner.next();
-            switch (shopAdd) {
-                case ("1"):
+            String end = "завершить";
+                if (shopAdd.equals("1")) {
                     price = 50000.50 + price;
-                    name = "iPhone X " + name;
+                    name = "iPhone X; " + name;
                     System.out.println(name + " добавлено в корзину\n");
-                    break;
-                case ("2"):
+                } else if (shopAdd.equals("2")) {
                     price = 70000.00 + price;
-                    name = "iPad 8 " + name;
+                    name = "iPad 8; " + name;
                     System.out.println(name + " добавлено в корзину\n");
-                    break;
-                case ("3"):
+                } else if (shopAdd.equals("3")) {
                     price = 20000.90 + price;
-                    name = "iPods 3 " + name;
+                    name = "iPods 3; " + name;
                     System.out.println(name + " добавлено в корзину\n");
-                    break;
-                case ("0"):
+                } else if (((shopAdd.equals("0")) || (shopAdd.equalsIgnoreCase(end)))) {
                     System.out.println("До свидания, заходите еще!\n");
-                    System.exit(0); // такой выход мы еще не проходили, подсмотрел в Ынтырнете
                     break;
-                default:
+                } else {
                     System.out.println("Вы ничего не выбрали\n");
-                    break;
+                }
+                System.out.println("У вас в корзине: " + name);
+                System.out.println(String.format("На сумму: %.2f\n", price));
+                System.out.println("Хотите приобрести что-то еще?");
             }
-            System.out.println("У вас в корзине: " + name);
-            System.out.println(String.format("На сумму: %.2f\n", price));
         }
     }
-}
+
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,21 +6,9 @@ public class Main {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
         Counter counter = new Counter();
-        Scanner scanner = new Scanner(System.in);
-        double needToPay;
         int persons = 0;
-        int rand = 0;
-        rand = counter.randomizeIt();
-        System.out.println("Большое спасибо, что посетили наш ресторан!\n");
-        System.out.println("Ваш счёт составил: " + rand + "руб.\n");
-        System.out.println("На скольких персон делить счёт?");
-        persons = counter.pers(persons);
-        needToPay = counter.count(rand, persons);
-        System.out.println("Каждый из вас должен: " + needToPay + " руб.\n");
-        System.out.println("Выберите себе игрушку из списка:");
-        System.out.println("1 - iPhone X\n2 - iPad 8\n3 - iPods 3\n0 - Выход");
+        counter.pers(persons);
         counter.shopCalc();
-
 
 
     }
