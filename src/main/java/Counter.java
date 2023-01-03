617e8a3b72769aa7736d560ec80abb7bcac16a64
sn00pg0d <madmaks.45cal@gmail.com>
2023-01-03 22:05:11 +0900

    Исправлены некоторые ошибки из замечаний ревьювера (03/01/23)

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,45 +1,102 @@
 import java.util.Scanner;
 
 public class Counter {
-    // создал класс как считалку, чтобы не захламлять main, но устроил бедлам здесь
-    // после автовыравнивания (альт + ктрл + L) стало немного получше
-    // да-да.. тему "Декомпозиция" я вообще не усвоил, а сейчас боюсь тут всё менять, сдам так В)
-    public static int pers() { // это запоминает кол-во человек введенных с клавы
+
+    public static int pers() { // Исправлено, с учетом замечания
         Scanner scanner = new Scanner(System.in);
         int persons = 0;
         System.out.println("Введите количество человек");
         while (persons <= 1) {
-            persons = scanner.nextInt();
-            if (persons <= 1) {
+            if (scanner.hasNextInt()) {
+                persons = scanner.nextInt();
+                if (persons <= 1) {
+                    System.out.println("Количество человек должно быть больше одного");
+                    System.out.println("--------------------------------------------\n");
+                    return pers();
+                }
+            } else {
                 System.out.println("Некорректное число, пожалуйста повторите ввод");
+                System.out.println("----------------------------------------------\n");
+                return pers();
             }
         }
         return persons;
     }
 
+    public static double enterPrice() {
+        Scanner scannerDb = new Scanner(System.in);
+        double price;
+        while (true) {
+            System.out.println("ВВедите стоимость товара:");
+            if (scannerDb.hasNextDouble()) {
+                price = scannerDb.nextDouble();
+                System.out.printf("Стоимость товара: %.2f\n", price);
+                System.out.println("-----------------------------\n");
+                return price;
+            }
+            System.out.println("Надо ввести цифры в формате рубль,копейки\n");
+            return enterPrice();
+        }
+        //return price;
+    }
+
     public static void shopCalc() { // это калькулятор суммы выбранных товаров и вообще всего
-        Scanner scanner = new Scanner(System.in);
-        String shopAdd = "";
-        String name = "";
-        double price = 0;
+        Scanner scannerStr = new Scanner(System.in);
+        String shopAdd;
+        String basket = "";
+        double sum = 0;
         int persons = pers();
-        System.out.println("Выберите себе игрушку из списка:");
-        System.out.println("1 - iPhone X\n2 - iPad 8\n3 - iPods 3\n0 или Завершить - Выход");
-        while (price != -1) { // бесконечный цикл, с выходом через "завершить"
-            shopAdd = scanner.next();
-            String end = "завершить";
-            if (shopAdd.equals("1")) {
-                price = 50000.50f + price;
-                name = "iPhone X; " + name;
-                System.out.println(name + " добавлено в корзину\n");
-            } else if (shopAdd.equals("2")) {
-                price = 70000.00f + price;
-                name = "iPad 8; " + name;
-                System.out.println(name + " добавлено в корзину\n");
-            } else if (shopAdd.equals("3")) {
+        while (true) { // Исправлено, с учетом замечания
+            System.out.println("Введите название товара:");
+            if (scannerStr.hasNext()) {
+                shopAdd = scannerStr.next();
+                String end = "завершить";
+                if (shopAdd.equalsIgnoreCase(end)) {
+                    System.out.println("\n-------------");
+                    System.out.println("Закрытие программы");
+                    break;
+                }
+                System.out.printf("%s добавлено в корзину\n", shopAdd);
+                System.out.println("-----------------------------\n");
+                //enterPrice();
+                double price = enterPrice();
+
+                sum = sum + price;
+                basket = basket + shopAdd;
+                System.out.printf("Добавленные товары: %s\n ", basket);
+                System.out.printf("На сумму: %.2f\n", sum);
+                System.out.println("\nХотите приобрести что-то еще?");
+                System.out.println("----------------------------------\n");
+            }
+
+
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*} else if (shopAdd.equals("3")) {
                 price = 20000.90f + price;
                 name = "iPods 3; " + name;
-                System.out.println(name + " добавлено в корзину\n");
+                System.out.println(name + " добавлено в корзину");
+                System.out.println("-----------------------------\n");
             } else if (((shopAdd.equals("0")) || (shopAdd.equalsIgnoreCase(end)))) {
                 double otvet = price / persons;
                 // я честно не понял как сделать через floor(), мы разве проходили такое?
@@ -47,21 +104,30 @@ public class Counter {
                 String rubles = String.valueOf(otvet);
                 if (rubles.endsWith("0")) { // endsWith нашел в контекстном меню, хз что это но сработало -_-
                     double yaWord = Double.parseDouble(String.valueOf(rubles)); // Double.parseDouble подсказал сам Андроид Студио, вроде тож работает
+                    System.out.println("---------------------------------------------------------\n");
                     System.out.println(String.format("Каждый из вас должен заплатить: %.2f рубля\n", yaWord));
                     System.out.println("До свидания, заходите еще!");
                     break;
                 }
                 double bWord = Double.parseDouble(String.valueOf(rubles));
+                System.out.println("---------------------------------------------------------\n");
                 System.out.println(String.format("Каждый из вас должен заплатить: %.2f рубль\n", bWord));
                 System.out.println("До свидания, заходите еще!");
                 break;
             } else {
-                System.out.println("Вы ничего не выбрали\n");
-            }
-            System.out.println("Добавленные товары: " + name);
-            System.out.println(String.format("На сумму: %.2f\n", price));
-            System.out.println("Хотите приобрести что-то еще?");
-        }
-    }
-}
-// ну вроде бы всё, спасибо за проверку! до встречи :)
+                System.out.println("Вы ничего не выбрали");
+                System.out.println("---------------------\n");*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
-public class Main { // обожаю минимализм
-    public static void main(String[] args) {
-        Counter counter = new Counter();
-        counter.shopCalc(); // запуск кол-ва человек и калькулятора корзины
-    }
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Counter.shopCalc(); // Исправлено, с учетом замечания
+
+    }
 }
